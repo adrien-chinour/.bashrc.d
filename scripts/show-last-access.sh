9190d628fd5534cb5233a81e3cb5fd705692e835
Adrien Chinour <git@chinour.fr>
2020-05-30 15:59:11 +0200

    remove unused scripts

--- scripts/show-last-access.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wholast() {
-    sudo tail -n 10 /var/log/nginx/*.access.log
-}
--- scripts/show-last-error.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-whofuck() {
-    sudo tail -n 10 /var/log/nginx/*.error.log
-}
